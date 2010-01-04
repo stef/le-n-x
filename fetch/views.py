777de5bce18b0738d2f5e7ad4bd5d5e0f2d110bb
stef <stefan.marsiske@gmail.com>
2010-01-04 15:05:51 +0100

    [enh] removed obsolete style in tagcloud response

--- fetch/views.py
+++ fetch/views.py
@@ -34,6 +34,6 @@ def handler(request):
         text = urllib2.urlopen(url)
         cloud=stem.tagcloud(text,25)
 
-        return HttpResponse('<link href="style.css" type="text/css" rel="stylesheet">\n%s' % (unicode(str(cloud),'utf8')))
+        return HttpResponse('%s' % (unicode(str(cloud),'utf8')))
     else:
         return render_to_response('fetch.html', { 'form': form, })
