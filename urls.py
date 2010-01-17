264637f9645f8541e1ef59f284f713a9f37712c3
stef <stefan.marsiske@gmail.com>
2010-01-17 21:51:42 +0100

    [mod] added backward compat url. will be phased out in a couple of months.

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ urlpatterns = patterns('',
     # Example:
     #(r'^tagcloud/(?P<url>.+)/$', 'ucloud.tagcloud.views.handler'),
     (r'^tagcloud/$', 'lenx.tagcloud.views.handler'),
+    (r'^ucloud/$', 'lenx.tagcloud.views.handler'),
     (r'^pippi/$', 'lenx.pippy.views.pippi'),
     (r'^xpippi/$', 'lenx.pippy.views.xpippi'),
 )
