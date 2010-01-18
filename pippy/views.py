d9b9870b09d750f616512844450814b38b710d64
stef <stefan.marsiske@gmail.com>
2010-01-19 00:13:10 +0100

    [enh] added coloring of diffs in xpippi and pippi. also a css stylesheet is now served.

--- pippy/views.py
+++ pippy/views.py
@@ -21,6 +21,8 @@ from django.shortcuts import render_to_response
 from django import forms
 from brain.document import MatchDb, Doc
 
+CSSHEADER='<head><link href="http://www.ctrlc.hu/~stef/pippi.css" media="screen" rel="stylesheet" title="" type="text/css"  /></head>'
+
 class PippiForm(forms.Form):
     doc1 = forms.CharField(required=True)
     doc2 = forms.CharField(required=True)
@@ -32,6 +34,21 @@ class XpippiForm(forms.Form):
 def htmlPippi(doc,matches,frag):
     return u'<span class="doc">in %s</span>: <span class="pos">%s</span><div class="txt">%s</div>' % (doc, matches, frag.decode('utf8'))
 
+def diffFrag(frag1,frag2):
+    match=True
+    i=0
+    while i<len(frag2):
+        if frag1[i].lower()!=frag2[i].lower() and match:
+            frag2[i]=u'<span class="diff">'+frag2[i]
+            match=False
+        elif frag1[i].lower()==frag2[i].lower() and not match:
+            frag2[i-1]=frag2[i-1]+u'</span>'
+            match=True
+        elif i==len(frag2)-1 and not match:
+            frag2[i-1]=frag2[i-1]+u'</span>'
+        i=i+1
+    return frag2
+
 def htmlRefs(this,docs):
     refs=sorted(this.refs.items(),
                 reverse=True,
@@ -40,17 +57,22 @@ def htmlRefs(this,docs):
     for (stem,ref) in refs:
         if len(stem) < 5: break
         columns=(int(100)/(len(ref['refs'])+1))
+        start=ref['matches'][0][0]
+        length=ref['matches'][0][1]
         res.append(u'<table class="frag" width="100%"><tr>')
         res.append(u'<td style="width:%d%%;">' % columns)
         res.append(htmlPippi(this.id,
                     ref['matches'],
-                    this.getFrag(ref['matches'][0][0],ref['matches'][0][1])))
+                    this.getFrag(start,length)))
         res.append(u'</td>')
+        origfrag=this.tokens[start:start+length]
         for doc in ref['refs']:
             d=docs[doc]
             m=d.refs[stem]['matches']
             res.append(u'<td style="width:%d%%;">' % columns)
-            res.append(htmlPippi(doc, m, d.getFrag(m[0][0],m[0][1])))
+            frag=d.tokens[m[0][0]:m[0][0]+m[0][1]]
+            frag=" ".join(diffFrag(origfrag,frag)).encode("utf8")
+            res.append(htmlPippi(doc, m, frag))
             res.append(u'</td>')
         res.append(u'</tr></table><hr />')
     return '\n'.join(res).encode('utf8')
@@ -59,10 +81,15 @@ def htmlLongFrags(this):
     frags=this.longestFrags()
     res=[]
     for (k,docs) in frags:
+        start=docs[0][1]
+        length=docs[0][2]
+        origfrag=this.docs[docs[0][0]].tokens[start:start+length]
         res.append(u'<table width="100%" class="frag"><tr>')
         for d in docs:
             res.append((u'<td style="width: %d%%;">' % (int(100)/len(docs))))
-            res.append(htmlPippi(d[0],d[1:],this.docs[d[0]].getFrag(d[1],d[2])))
+            frag=this.docs[d[0]].tokens[d[1]:d[1]+d[2]]
+            frag=" ".join(diffFrag(origfrag,frag)).encode("utf8")
+            res.append(htmlPippi(d[0],d[1:],frag))
             res.append(u'</td>')
         res.append(u'</tr></table><hr />')
     return '\n'.join(res).encode('utf8')
@@ -78,7 +105,7 @@ def pippi(request):
         db.addDoc(d2)
         result=htmlLongFrags(db)
 
-        return HttpResponse('%s' % (unicode(str(result),'utf8')))
+        return HttpResponse('%s\n%s' % (CSSHEADER,unicode(str(result),'utf8')))
     else:
         return render_to_response('pippi.html', { 'form': form, })
 
@@ -95,6 +122,7 @@ def xpippi(request):
             if d:
                 db.insert(d)
             result=htmlRefs(d,db.docs)
-        return HttpResponse('%s' % (unicode(str(result),'utf8')))
-    return render_to_response('xpippi.html', { 'form': form, })
+        return HttpResponse('%s\n%s' % (CSSHEADER,unicode(str(result),'utf8')))
+    else:
+        return render_to_response('xpippi.html', { 'form': form, })
 
