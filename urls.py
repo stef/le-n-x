20c275c139c4466bbc9daa8c2e32d5a6c9b6f310
stef <stefan.marsiske@gmail.com>
2010-01-24 23:46:45 +0100

    [enh] more robust document view. added bonus: persistent database.

--- urls.py
+++ urls.py
@@ -1,4 +1,7 @@
 from django.conf.urls.defaults import *
+from brain.document import MatchDb
+db=MatchDb()
+db.load()
 
 urlpatterns = patterns('',
     # Example:
@@ -8,7 +11,7 @@ urlpatterns = patterns('',
     (r'^pippi/$', 'lenx.pippy.views.pippi'),
     (r'^xpippi/$', 'lenx.pippy.views.xpippi'),
     (r'^stats/$', 'lenx.stats.views.stats'),
-    (r'^view/$', 'lenx.view.views.viewPippiDoc'),
-    (r'^view/(?P<cutoff>\d+)/(?P<doc>.+)/$', 'lenx.view.views.viewPippiDoc'),
-    (r'^view/(?P<doc>.+)/$', 'lenx.view.views.viewPippiDoc'),
+    (r'^view/$', 'lenx.view.views.viewPippiDoc',{'db': db}),
+    (r'^view/(?P<cutoff>\d+)/(?P<doc>.+)/$', 'lenx.view.views.viewPippiDoc',{'db': db}),
+    (r'^view/(?P<doc>.+)/$', 'lenx.view.views.viewPippiDoc',{'db': db}),
 )
--- view/views.py
+++ view/views.py
@@ -23,27 +23,52 @@ from django import forms
 from BeautifulSoup import BeautifulSoup, Tag
 import re, urllib
 import stopwords
-from brain.document import MatchDb, Doc
 
 CSSHEADER="""<head>
 <script type="text/javascript" charset="utf-8" src="http://ajax.googleapis.com/ajax/libs/jquery/1.4.0/jquery.min.js"></script>
 <link href="http://www.ctrlc.hu/~stef/pippi.css" media="screen" rel="stylesheet" title="" type="text/css"  />
 <script type="text/javascript">
          $(document).ready(function() {
-               /*$(".pippi").tooltip({cssClass: "",});*/
-               $('.right').click(function() { $(this).children().toggle('slow'); return false; }).children().hide();
+               $('.right .header').click(function() { $(this).siblings().toggle('slow'); return false; }).siblings().hide();
                });
       </script></head>"""
 
 #CSSHEADER='<head><link href="http://www.ctrlc.hu/~stef/pippi.css" media="screen" rel="stylesheet" title="" type="text/css"  /></head>'
 
-db=MatchDb()
-db.load()
-
 class pippiForm(forms.Form):
     doc = forms.CharField(required=True)
 
-def viewPippiDoc(request,doc=None,cutoff=7):
+def getNote(docs,soup,cutoff):
+    note = Tag(soup, "span", [("class","right")])
+    ul=Tag(soup,"ul")
+    note.insert(0,ul)
+    for doc in docs:
+        link=Tag(soup,"a", [("href","/view/%s/%s/" % (cutoff,doc))])
+        link.insert(0,doc)
+        li=Tag(soup,"li")
+        li.insert(0,link)
+        ul.insert(0,li)
+    span=Tag(soup,'span', [("class","header")])
+    span.insert(0,'Matches in (%s)' % len(docs))
+    note.insert(0,span)
+    return note
+
+def pippify(match,refs,soup,cutoff,regex):
+    container = []
+    segments=re.split(regex, match)
+    for text in segments:
+        if(re.match(regex,text)):
+            container.append(getNote(refs,soup,cutoff))
+            markedmatch = Tag(soup, "span", [("class","pippi"),('title','Matches in: %s' % ", ".join(refs))])
+            markedmatch.insert(0,text)
+            container.append(markedmatch)
+        else:
+            container.append(text)
+    return container
+
+def viewPippiDoc(request,doc=None,cutoff=7,db=None):
+    if not db:
+        return HttpResponse('Error: db none')
     form = pippiForm(request.GET)
     if not doc and form.is_valid():
         doc=form.cleaned_data['doc'].strip('\t\n')
@@ -56,42 +81,39 @@ def viewPippiDoc(request,doc=None,cutoff=7):
     meat=soup.find(id='TexteOnly')
     for (stem,ref) in sorted(d.refs.items(),cmp=lambda x,y: cmp(len(x[0]),len(y[0])),reverse=True):
         if stem in stopwords.stopfrags or len(stem)<int(cutoff): continue
-        start=ref['matches'][0][0]
-        end=start+ref['matches'][0][1]
-        regex=" ?".join(map(lambda x: (x[0] in ['(',')','\\','?','.','*','['] and '['+x+']') or x,d.tokens[start:end]))
-        for match in meat.findAll(text=re.compile(regex)):
-            parent=match.parent
-            if not parent.string: continue
-            (pre,m,post)=parent.string.partition(match)
-            parent.string.extract()
-            #br=Tag(soup, "br", [("style","clear: both;")] )
-            #parent.insert(0,br)
-            parent.insert(0,post)
-            markedmatch = Tag(soup, "span", [("class","pippi"),('title','Matches in: %s' % ", ".join(ref['refs']))])
-            markedmatch.insert(0,m)
-            parent.insert(0,markedmatch)
-            parent.insert(0,pre)
-            note = Tag(soup, "div", [("class","right")])
-            ul=Tag(soup,"ul")
-            note.insert(0,ul)
-            for odoc in ref['refs']:
-                link=Tag(soup,"a", [("href","/view/%s/%s/" % (cutoff,odoc))])
-                link.insert(0,odoc)
-                li=Tag(soup,"li")
-                li.insert(0,link)
-                ul.insert(0,li)
-            note.insert(0,'Matches in (%s)' % len(ref['refs']))
-            parent.insert(0,note)
-    for match in meat.findAll(text=re.compile("^[Aa]rticle [0-9.,]*$")):
-        parent=match.parent
-        if not parent.string: continue
-        a=Tag(soup,'a',[('name',urllib.quote(unicode(match)))])
-        a.insert(0,parent.string)
-        parent.insert(0,a)
-    #result='<ul class="topfrags">'
-    #result+=", ".join(["<li>%s%s</li>" % (len(x),"<ul>"+d.refs[x]['refs'])+"</ul>") for x in sorted(d.refs.keys(),reverse=True,cmp=lambda x,y: cmp(len(x),len(y))) if len(x)>cutoff])
-    #result+="</ul>"
-    result='<div class="doc">'
+        for (start,length) in ref['matches']:
+            regex=re.compile('('+"\s*".join(
+                map(lambda x: re.escape(x),
+                    d.tokens[start:start+length]))+')',
+                re.I | re.M)
+            try:
+                node=meat.find(text=regex)
+            except:
+                # all matches possibly eaten by greedy pippifying below
+                continue
+            while node:
+                nodeidx=node.parent.contents.index(node)
+                pippies=pippify(node,ref['refs'],soup,cutoff,regex)
+                for pippi in reversed(pippies):
+                    node.parent.insert(nodeidx,pippi)
+                n=node
+                node=node.findNextSibling(text=regex)
+                n.extract()
+    #for match in meat.findAll(text=re.compile("^[Aa]rticle [0-9.,]*$")):
+    #    # TODO better article naming
+    #    parent=match.parent
+    #    if not parent.string: continue
+    #    a=Tag(soup,'a',[('name',urllib.quote(unicode(match)))])
+    #    a.insert(0,parent.string)
+    #    parent.insert(0,a)
+    # TODO add header with all relevant documents
+    # TODO add header tagcloud fo this document
+    #result+='<div><ul class="right">'
+    #result+="".join([ "<li>%s%s</li>" % (len(x),d.refs[x]['refs'])
+    #                    for x in sorted(d.refs.keys(),reverse=True,cmp=lambda x,y: cmp(len(x),len(y)))
+    #                    if len(x)>cutoff])
+    #result+="</ul></div>"
+    result+='<div class="doc">'
     result+=str(meat)
     result+='</div>'
-    return HttpResponse('%s\n%s' % (CSSHEADER,unicode(str(result),'utf8')))
+    return HttpResponse('%s\n%s' % (CSSHEADER,unicode(result,'utf8')))
