edcc947ec7da6b9c2f3927af03f29c1aee4c22fc
stef <stefan.marsiske@gmail.com>
2010-02-03 01:16:40 +0100

    [enh] conserve memory.

--- brain/bulkproducer.py
+++ brain/bulkproducer.py
@@ -17,14 +17,44 @@
 # (C) 2010 by Stefan Marsiske, <stefan.marsiske@gmail.com>
 
 import sys
+from itertools import izip, count
+
+try:
+    from itertools import combinations
+except ImportError:
+    def combinations(iterable, r):
+        # combinations('ABCD', 2) --> AB AC AD BC BD CD
+        # combinations(range(4), 3) --> 012 013 023 123
+        pool = tuple(iterable)
+        n = len(pool)
+        if r > n:
+            return
+        indices = range(r)
+        yield tuple(pool[i] for i in indices)
+        while True:
+            for i in reversed(range(r)):
+                if indices[i] != i + n - r:
+                    break
+            else:
+                return
+            indices[i] += 1
+            for j in range(i+1, r):
+                indices[j] = indices[j-1] + 1
+            yield tuple(pool[i] for i in indices)
 
 alldocs=[x.strip('\t\n') for x in sys.stdin]
-l=len(alldocs)*(len(alldocs)-1)/2
-allops=[(x,y) for x,i in zip(alldocs,xrange(l)) for y in alldocs[i+1:]]
+#l=len(alldocs)*(len(alldocs)-1)/2
+#allops=[(x,y) for x,i in zip(alldocs,xrange(l)) for y in alldocs[i+1:]]
 
 idx=int(sys.argv[1])
 clusters=int(sys.argv[2])
 
-start=idx*l/clusters
-end=start+l/clusters
-print "\n".join([str(x) for x in allops][start:end])
+counter=count(0)
+combis=combinations(alldocs,2)
+try:
+    job=combis.next()
+    while job:
+        if (counter.next() % clusters)==idx: print job
+        job=combis.next()
+except StopIteration:
+    pass
