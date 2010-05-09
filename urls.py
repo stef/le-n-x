7e0fc768cb908a8816e59df19033b5e69b985424
stef <stefan.marsiske@gmail.com>
2010-05-10 01:52:50 +0200

    [mod] removed obsolete url

--- urls.py
+++ urls.py
@@ -9,7 +9,6 @@ urlpatterns = patterns('',
     (r'^xpippi/(?P<doc>.+)', view.xpippi),
     (r'^all$', view.listDocs),
     (r'^stats$', view.stats),
-    #(r'^view$', view.viewPippiDoc),
     (r'^doc/(?P<doc>.+)$', view.docView),
 )
 
