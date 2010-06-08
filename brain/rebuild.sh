3f9d2b17723a04c19edc6da953293e8e6a344a5f
asciimoo <asciimoo@gmail.com>
2010-06-08 19:21:56 +0200

    [enh][fix] rebuild.sh update, closing > for a docView html tag

--- brain/rebuild.sh
+++ brain/rebuild.sh
@@ -7,13 +7,11 @@
 PPATH=`dirname $0`'/..'
 
 # mongo db delete
-printf "use pippi\ndb.miscdb.drop()\ndb.docs.drop()\ndb.pippies.drop()\nexit\n" | mongo
+printf "use pippi\ndb.miscdb.drop()\ndb.docs.drop()\ndb.pippies.drop()\ndb.frags.drop()\nexit\n" | mongo
+
 
 # load docs to pippify
-(ls $1 | grep $3 | ksh $PPATH/brain/bulkloaddoc.sh $2 2>&1; cd -) | less
-
-# save processed items
-PYTHONPATH=$PPATH/../ DJANGO_SETTINGS_MODULE="lenx.settings" python $PPATH/brain/bulksaver.py
+(ls $1 | grep $3'' | ksh $PPATH/brain/bulkloaddoc.sh $2 2>>$PPATH/brain/bulk.err; cd -) 
 
 # calculate/store tf-idf for processed docs
 PYTHONPATH=$PPATH/../ DJANGO_SETTINGS_MODULE="lenx.settings" python $PPATH/brain/bulkidf.py
