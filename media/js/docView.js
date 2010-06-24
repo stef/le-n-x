5c8db8672bd43d164f86b745895002a3db4bedfd
asciimoo <asciimoo@gmail.com>
2010-06-24 19:01:44 +0200

    [enh] msgbox javascript basics added

--- media/js/docView.js
+++ media/js/docView.js
@@ -1,25 +1,67 @@
+  var msgFocus=0, hlFocus=0;
   Array.prototype.contains = function (element) {
     for (var i = 0; i < this.length; i++) {
       if (this[i] == element) { return true; }
     }
     return false; };
-
+  
+  function MsgBox(id, idleTime) {
+      this.e = $(id);
+      this.aborto = new Date();
+      this.abortc = new Date();
+      this.it = idleTime;
+      this.ct = 0; // close timer
+      this.show = function(c) { 
+          if((new Date())-this.aborto > this.it) {
+              if(!$(this.e).is(':visible')) {
+                  $(this.e).fadeIn('slow');
+                  $('#msgbox_c').html(c);
+              } else {
+                  $('#msgbox_c').append('<hr />'+c);
+              }
+          }
+      }
+      this.timedShow = function(c) {
+          var a = this;
+          setTimeout( function() { a.show(c); }, this.it);
+      }
+      this.doHide = function(){  if((new Date())-this.abortc > this.ct) this.e.fadeOut(); }
+      this.hide = function(t) {
+          this.ct = t;
+          var a = this;
+          setTimeout(function() { a.doHide(); } , t);
+      }
+      this.abortClose = function() {
+          console.log("abortClose called");
+          this.abortc = new Date();
+      }
+      this.abortOpen = function() {
+          console.log("abortOpen called");
+          this.aborto = new Date();
+      }
+  }
   $(document).ready(function() {
-    $('#loading').show();
-
+     $('#loading').show();
+     var msgBox = new MsgBox('#msgbox', 300);
+     $('#msgbox').hover(function() { msgBox.abortClose(); }, function() { msgBox.hide(1000); });
      $('.highlight').hover(
          function() {
          $.map($('.'+$(this).attr('class').split(' ')[1]),
              function(e) {
                  $(e).addClass('hovered');
              });
+         msgBox.abortClose();
+         msgBox.timedShow($(this).html());
          },
          function(){
          $.map($('.'+$(this).attr('class').split(' ')[1]),
              function(e) {
                  $(e).removeClass('hovered');
              });
-         });
+         msgBox.abortOpen();
+         msgBox.hide(2000);
+         }
+     );
 	 $(".highlight").each(function(){
 	 	var el = $(this);
 
